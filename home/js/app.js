ecf9407c040dfa38b27f15b562eb674d64d7aa45
SyydMR <mohammadrezadaryabaki@gmail.com>
2023-11-27 11:20:43 +0330

    for push project to github

---
+++ home/js/app.js
@@ -0,0 +1,15 @@
+const menuIcon = document.querySelector('.menu-logo');
+const menuBurger = document.querySelector('.menu-burger');
+const searchIcon = document.querySelector('.search-logo');
+const searchField = document.querySelector('.search-field')
+menuIcon.addEventListener("click", showList);
+searchIcon.addEventListener("click", showField);
+
+function showList() {
+    menuBurger.classList.toggle('show');
+
+}
+
+function showField() {
+    searchField.classList.toggle('show');
+}
