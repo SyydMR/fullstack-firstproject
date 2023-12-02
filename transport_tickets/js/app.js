c75f0a12ffd6070e40c3a02c899ddbaacdb30f7b
SyydMR <mohammadrezadaryabaki@gmail.com>
2023-12-01 19:45:24 -0700

    update trasport-ticket page 'front'

--- transport_tickets/js/app.js
+++ transport_tickets/js/app.js
@@ -21,36 +21,43 @@ function applyTranform1() {
   document.getElementById("lbl1").style.transform = "translate(0, 0)";
   document.getElementById("lbl1").style.color = "black";
   document.getElementById("lbl1").style.transition = "0.5s all";
+  document.getElementById("lbl1").style.fontSize = "24px";
 }
 
 function applyTranform2() {
   document.getElementById("lbl2").style.transform = "translate(0, 0)";
   document.getElementById("lbl2").style.color = "black";
   document.getElementById("lbl2").style.transition = "0.5s all";
+  document.getElementById("lbl2").style.fontSize = "24px";
 }
 
 function applyTranform3() {
   document.getElementById("lbl3").style.transform = "translate(0, 0)";
   document.getElementById("lbl3").style.color = "black";
   document.getElementById("lbl3").style.transition = "0.5s all";
+  document.getElementById("lbl3").style.fontSize = "24px";
 }
 
 function applyTranform4() {
   document.getElementById("lbl4").style.transform = "translate(0, 0)";
   document.getElementById("lbl4").style.color = "black";
   document.getElementById("lbl4").style.transition = "0.5s all";
+  document.getElementById("lbl4").style.fontSize = "24px";
 }
 
 function applyTranform5() {
   document.getElementById("lbl5").style.transform = "translate(0, 0)";
   document.getElementById("lbl5").style.color = "black";
   document.getElementById("lbl5").style.transition = "0.5s all";
+  document.getElementById("lbl5").style.fontSize = "24px";
+
 }
 
 function applyTranform6() {
   document.getElementById("lbl6").style.transform = "translate(0, 0)";
   document.getElementById("lbl6").style.color = "black";
   document.getElementById("lbl6").style.transition = "0.5s all";
+  document.getElementById("lbl6").style.fontSize = "24px";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -71,34 +78,66 @@ document.addEventListener("DOMContentLoaded", function () {
     if (inputElement1.value === "") {
       labelElement1.style.transform = "translate(20px, 40px)";
       labelElement1.style.color = "#a8adb3";
+      labelElement2.style.fontSize = "20px";
+      
     }
   });
   inputElement2.addEventListener("blur", function () {
     if (inputElement2.value === "") {
       labelElement2.style.transform = "translate(20px, 40px)";
       labelElement2.style.color = "#a8adb3";
+      labelElement2.style.fontSize = "20px";
+
     }
   });
   inputElement3.addEventListener("blur", function () {
     if (inputElement3.value === "") {
       labelElement3.style.transform = "translate(20px, 40px)";
-      labelElement3.style.color = "#a8adb3";    }
+      labelElement3.style.color = "#a8adb3";
+      labelElement3.style.fontSize = "20px";
+
+    }
   });
   inputElement4.addEventListener("blur", function () {
     if (inputElement4.value === "") {
       labelElement4.style.transform = "translate(20px, 40px)";
       labelElement4.style.color = "#a8adb3";
+      labelElement4.style.fontSize = "20px";
+
     }
   });
   inputElement5.addEventListener("blur", function () {
     if (inputElement5.value === "") {
       labelElement5.style.transform = "translate(20px, 40px)";
-      labelElement5.style.color = "#a8adb3";    }
+      labelElement5.style.color = "#a8adb3";
+      labelElement5.style.fontSize = "20px";
+    }
   });
   inputElement6.addEventListener("blur", function () {
     if (inputElement6.value === "") {
       labelElement6.style.transform = "translate(20px, 40px)";
-      labelElement6.style.color = "#a8adb3";    
+      labelElement6.style.color = "#a8adb3";
+      labelElement6.style.fontSize = "20px";
     }
   });
 });
+
+document.getElementById("radio-1").addEventListener("click", myFunction1, true);
+document.getElementById("radio-2").addEventListener("click", myFunction2, true);
+document.getElementById("radio-3").addEventListener("click", myFunction3, true);
+
+function myFunction1(){
+  document.querySelector(".glider").style.backgroundColor = "#df3e3e";
+  document.querySelector(".submit-btn").style.backgroundColor = "#df3e3e";
+
+}
+function myFunction2(){
+  document.querySelector(".glider").style.backgroundColor = "#a8adb3";
+  document.querySelector(".submit-btn").style.backgroundColor = "#a8adb3";
+
+}
+function myFunction3(){
+  document.querySelector(".glider").style.backgroundColor = "rgba(34, 200, 206, 0.7)";
+  document.querySelector(".submit-btn").style.backgroundColor = "rgba(34, 200, 206, 0.7)";
+
+}
